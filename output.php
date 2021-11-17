572f5a98962921329b69c48876fbe6bb7995aedd
Andex <anyanov1998@gmail.com>
2021-11-17 15:48:18 +0300

    Delete: unnecessary

--- output.php
+++ output.php
@@ -1,11 +1,6 @@
 <?php
-
 session_start();
-//unset($_SESSION['user_id']);
-//unset($_SESSION['username']);
-//setcookie('username', md5($user_id.$login), time() - 10, '/');
-//setcookie('user_id', $user_id, time() - 10, '/');
 session_destroy();
 header("Location: input.php");
 exit();
-//?>
+?>
